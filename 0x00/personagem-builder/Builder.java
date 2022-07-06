436ddabe73bed0487abf6b4493db84b6a0e645a5
Renan de Lima Gomes <renanlg@ciandt.com>
2022-07-06 08:37:42 -0300

    1. Construtor de Personagens

---
+++ 0x00/personagem-builder/Builder.java
@@ -0,0 +1,10 @@
+public interface Builder {
+
+    void setNome(String nome);
+    void setTipo(TipoPersonagem tipoPersonagem);
+    void setForca(int forca);
+    void setInteligencia(int inteligencia);
+    void setVigor(int vigor);
+    void setResistencia(int resistencia);
+    void setDestreza(int destreza);
+}
