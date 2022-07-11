5bb57f9b21995c20c369486dd2384e404c9dddde
Renan de Lima Gomes <renanlg@ciandt.com>
2022-07-11 05:23:21 -0300

    0. Guerra de Naves Espaciais

---
+++ 0x03/naves/NaveEspacial.java
@@ -0,0 +1,22 @@
+public class NaveEspacial {
+    private int saude;
+    private int ataque;
+
+    public NaveEspacial(int saude, int ataque) {
+        this.saude = saude;
+        this.ataque = ataque;
+    }
+
+    public int getSaude() {
+        return saude;
+    }
+
+    public int getAtaque() {
+        return ataque;
+    }
+
+    @Override
+    public String toString() {
+        return "Saude: " + getSaude() + " - Ataque: " + getAtaque();
+    }
+}
